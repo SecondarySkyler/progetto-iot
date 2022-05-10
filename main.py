144f744d7fa26c514e0ddcf24f5db268963a0c78
SecondarySkyler <skylerhunter6@gmail.com>
2022-05-10 16:49:56 +0200

    added utils

---
+++ main.py
@@ -0,0 +1,15 @@
+from influxdb import InfluxDBClient
+import time
+import temp_sensor
+
+SHUTDOWN = False
+
+client = InfluxDBClient(host = 'localhost', port = 8086, user = 'python', password = 'pass')
+
+while not SHUTDOWN:
+    temp = temp_sensor.read_temp()
+    print(temp)
+    time.sleep(5)
+
+
+
--- temp_sensor.py
+++ temp_sensor.py
@@ -7,13 +7,13 @@ import glob #usefull for search directory with jolly * character
 BASE_DIR = '/sys/bus/w1/devices/'
 SENSOR_DIR = glob.glob(BASE_DIR + '28*')[0]
 FULLPATH = os.path.join(BASE_DIR, SENSOR_DIR)
-FILE = FULLPATH + '/w1_slave'
+FILE = FULLPATH + '/temperature'
 
 
 def read_temp():
     file = open(FILE, 'r')
     lines = file.readlines()
-    f.close
+    file.close()
     return lines
 
 print(read_temp())
