f1930040902347d32fad351817a143e615624542
SecondarySkyler <skylerhunter6@gmail.com>
2022-05-11 13:47:20 +0200

    Added new module for other sensors

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from influxdb import InfluxDBClient
 import time
-import temp_sensor
+#import temp_sensor
+import utils
 
 SHUTDOWN = False
 
-client = InfluxDBClient(host = 'localhost', port = 8086, user = 'python', password = 'pass')
+#client = InfluxDBClient(host = 'localhost', port = 8086, user = 'python', password = 'pass')
 
-while not SHUTDOWN:
-    temp = temp_sensor.read_temp()
-    print(temp)
-    time.sleep(5)
+#while not SHUTDOWN:
+ #   temp = temp_sensor.read_temp()
+ #   print(temp)
+ #   time.sleep(5)
+
 
 
 
