ba5ca28abdbb82444ef4d1a39af552ef7ab6b75f
SecondarySkyler <skylerhunter6@gmail.com>
2022-05-17 15:45:43 +0200

    Added tds

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from influxdb import InfluxDBClient
 import time
 import temp_sensor
 import turbidity_sensor
+import tds_sensor
 import utils
 
 SHUTDOWN = False
@@ -13,10 +14,11 @@ data = []
 while not SHUTDOWN:
    temp = temp_sensor.read_temp()
    turb = turbidity_sensor.read_turbidity()
+   tds = tds_sensor.read_tds()
    data.append(utils.parse_json('Temperatura', 'gradi', temp))
    data.append(utils.parse_json('Torbidita', 'ntu', turb))
    data.append(utils.parse_json('pH', 'pH', 5))
-   data.append(utils.parse_json('TDS', 'residuo fisso', 23))
+   data.append(utils.parse_json('TDS', 'residuo fisso', tds))
    
    client.write_points(data, database = 'progetto', protocol = 'json')
    time.sleep(5)
