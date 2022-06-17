26efe3c89d1a536edfb248c20482aae6783278dd
SecondarySkyler <skylerhunter6@gmail.com>
2022-06-17 10:55:27 +0200

    migration to arduino

--- utils.py
+++
@@ -1,8 +0,0 @@
-def parse_json(measurement, field_name, value):
-    json = {
-       'measurement' : measurement,
-       'fields':  {
-           field_name : value
-       }
-    }
-    return json
